420b7e20ba3040ba864e3437086c9f72ceb67e60
shugoY45 <s.yoshida.aogiri45@gmail.com>
2020-10-10 23:37:46 +0900

    recreate special

--- make_shift/__init__.py
+++ make_shift/__init__.py
@@ -3,44 +3,45 @@ from make_shift.function import set_list, classify
 from make_shift.normal import normal
 from make_shift.special import special
 from make_shift.rest import rest
-from flask_schedule.models import Worker, Job
+from flask_schedule.models import Worker, Job, SpecialJob
 
 def main():
   workers = Worker.query.all()
   jobs = Job.query.all()
+  specialjobs = SpecialJob.query.all()
 
   indivshifts = set_list(workers)
 
   _ , spshifts = special(workers,specialjobs)
   # print(spshifts)
 
-  workers2, restshifts = rest(workers,spshifts)
-  # print(workers2)
-  # print(restshifts)
+  # workers2, restshifts = rest(workers,spshifts)
+  # # print(workers2)
+  # # print(restshifts)
 
-  shifts = restshifts + spshifts
-  indivshifts = classify(shifts,indivshifts)
+  # shifts = restshifts + spshifts
+  # indivshifts = classify(shifts,indivshifts)
 
 
-  workers3, _ = special(workers2,specialjobs)
-  # print(workers3)
+  # workers3, _ = special(workers2,specialjobs)
+  # # print(workers3)
 
-  normalshifts = normal(workers3,Jobs,indivshifts)
-  # print(normalshifts)
+  # normalshifts = normal(workers3,Jobs,indivshifts)
+  # # print(normalshifts)
 
-  shift = normalshifts
-  # shifts = normalshifts + restshifts + spshifts
+  # shift = normalshifts
+  # # shifts = normalshifts + restshifts + spshifts
 
-  # print(shifts)
+  # # print(shifts)
 
-  # indivshifts = Classify(shifts,Worker)
-  # indivshifts = Classify(shifts,indivshifts)
-  viewindivs = []
-  for indivs in indivshifts:
-    tmp = []
-    for inds in indivs:
-      tmp.append(ind[0])
-    viewindivs.append(tmp)
+  # # indivshifts = Classify(shifts,Worker)
+  # # indivshifts = Classify(shifts,indivshifts)
+  # viewindivs = []
+  # for indivs in indivshifts:
+  #   tmp = []
+  #   for inds in indivs:
+  #     tmp.append(ind[0])
+  #   viewindivs.append(tmp)
     
 
   # print(viewindivs)
--- make_shift/special.py
+++ make_shift/special.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import copy
 from setting import *
+from make_shift.module import Shift,Worker
 
 def special(workers,specialjobs):
   spshifts = []
@@ -10,32 +11,36 @@ def special(workers,specialjobs):
   for spjob in specialjobs:
     found = False
     for worker in workers:
-      if spjob[spworker_name] == worker[worker_name]:
+      if spjob.workername == worker.workername:
         found = True
         # 特別シフト作成
-        starttime = datetime.strptime(spjob[spjob_start_time], '%H:%M')
-        endtime = datetime.strptime(spjob[spjob_end_time], '%H:%M')
-        if datetime.strptime(worker[work_start_time], '%H:%M')<=starttime and endtime<=datetime.strptime(worker[work_end_time], '%H:%M'):
-          spshifts.append((worker[worker_name],spjob[spjob_name],spjob[spjob_start_time],spjob[spjob_end_time],spjob[spjob_weight]))
+        starttime = datetime.strptime(spjob.starttime, '%H:%M')
+        endtime = datetime.strptime(spjob.endtime, '%H:%M')
+        if datetime.strptime(worker.starttime, '%H:%M')<=starttime and endtime<=datetime.strptime(worker.endtime, '%H:%M'):
+          shift = Shift(worker.workername,spjob.jobname,spjob.starttime,spjob.endtime,spjob.priorty)
+          spshifts.append(shift)
 
           # 特別シフトの時間を外し、従業員リストの仕事時間を更新する。
-          newworkers.append((worker[worker_name],worker[work_start_time],spjob[spjob_start_time],0))
-          newworkers.append((worker[worker_name],spjob[spjob_end_time],worker[work_end_time],0))
+          newworker = Worker(worker.workername,worker.starttime,spjob.starttime)
+          newworkers.append(newworker)
+          newworker = Worker(worker.workername,spjob.endtime,worker.endtime)
+          newworkers.append(newworker)
           newworkers.remove(worker)
           break
-        # else :
+        else :
           # print(worker)
-          # raise Exception(str(spjob[spjob_start_time])+'から'+str(spjob[spjob_end_time])+'の時間に'+str(worker[worker_name])+'さんはいません')
+          raise Exception(str(spjob.starttime)+'から'+str(spjob.endtime)+'の時間に'+str(worker.workername)+'さんはいません')
         
 
     if not found:
-      print(spjob[spworker_name])
-      raise Exception('本日、'+str(spjob[spworker_name])+'さんは出勤ではありません')
-
-  return newworkers,spshifts
+      print(spjob.workername)
+      raise Exception('本日、'+str(spjob.workername)+'さんは出勤ではありません')
 
   # print(spshifts)
   # print(newworkers)
+
+  return newworkers,spshifts
+
 
 
 
