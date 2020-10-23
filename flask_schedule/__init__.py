cc73aa3efa4913fd12ac1b12c64e13c280e3a837
shugoY45 <s.yoshida.aogiri45@gmail.com>
2020-10-23 14:47:09 +0900

    add psql

--- flask_schedule/__init__.py
+++ flask_schedule/__init__.py
@@ -8,6 +8,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('flask_schedule.config')
+db_uri = "sqlite:///" + os.path.join(app.root_path, 'site.db') or "postgres://obwkcqkdnweeeq:57b1214817ae4efd7be6b26bed59a6835df968882f615aca9141fe7848f8ce6d@ec2-54-157-234-29.compute-1.amazonaws.com:5432/d12kknduts3889"
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 db = SQLAlchemy(app)
 
 # app.jinja_env = Environment(extensions=['jinja2.ext.loopcontrols'])
