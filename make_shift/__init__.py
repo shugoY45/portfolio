6fd1d9c399187850f544666509776202928d248b
shugoY45 <s.yoshida.aogiri45@gmail.com>
2021-02-04 13:37:03 +0900

    add comments

--- make_shift/__init__.py
+++ make_shift/__init__.py
@@ -5,45 +5,48 @@ from make_shift.function import evaluation
 import random
 
 def main(workers,jobs,spjobs,config):
-  #シフトの初期値の作成
-  # spshifts = special(workers,spjobs,config)
-  # restshifts = rest(workers,config)
-  # normalshifts = normal(workers,jobs,config)
-  # shifts = spshifts + restshifts + normalshifts
 
-  #目的関数を最小化する
-  # shifts = minimize(workers,jobs,spjobs,config)
+  #乱択山登り法を用いた最小化
 
-  #山登り法を用いた最小化
-  #スケジュールの点数を格納する
-  opentime = config.store_opentime
-  closetime = config.store_closetime
-  hour_list = [i for i in range(opentime.hour,closetime.hour)]
+  # 山登り法とは「現在の解の近傍の内で最も成績の良い解」を近傍解として選び、「現在の解より近傍解の成績の方が良い場合」に近傍解と現在の解を入れ換える方法であり、極値を見つけるのには適している。
+  # この山登り法を乱択アルゴリズムとして、ランダムに探索開始の初期値を複数選び、探索が終了し極値が見つかった後、見つけた極値の中から最小値･最大値を選ぶことにより目的関数の最小値・最大値として近似する。
+  # 本プログラムでは時間と従業員のlistの順番をランダムにすることでスケジューリングに多様性を持たせ、evoluate関数を用いて点数をつけることで最も良い点数のスケジュールを返している。
+  
   one_date = workers[0].one_date.date()
-
   minscore = 0
   normalshifts = []
 
-  for i in range(10):
+  # 時間配列作成
+  opentime = config.store_opentime
+  closetime = config.store_closetime
+  hour_list = [i for i in range(opentime.hour,closetime.hour)]
+
+  # 乱択アルゴリズムの初期値の個数
+  T = 100
+
+  for i in range(T):
     for worker in workers:
       worker.starttime = worker.starttime.time()
       worker.endtime = worker.endtime.time()
       worker.shift_init(one_date)
+    # 乱択のためのシャッフル
     random.shuffle(hour_list)
+    random.shuffle(workers)
+
+    # スケジュールの作成
     spshifts = special(workers,spjobs,config)
     restshifts = rest(workers,config)
     prenormalshifts = normal(workers,jobs,config,hour_list)
     preshifts = spshifts + restshifts + prenormalshifts
+
+    #スケジュール（preshifts）の点数をevoluate関数で評価し、最高得点の場合shiftsに格納。
     score = evaluation(preshifts,workers)
-    # print(score)
     if minscore == 0:
       minscore = score
       shifts = preshifts
     elif minscore > score:
       minscore = score
       shifts = preshifts
-  # print(minscore)
-  # print(shifts)
 
   return shifts
   
